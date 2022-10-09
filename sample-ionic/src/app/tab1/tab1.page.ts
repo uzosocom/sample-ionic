bc3694b8c625e041080cdd4599eed26417404b5e
uzosocom <7zu6s87y9fku@gmail.com>
2022-10-10 02:23:39 +0900

    feat: device sdkVersion

--- sample-ionic/src/app/tab1/tab1.page.ts
+++ sample-ionic/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Device } from '@awesome-cordova-plugins/device/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor(private device: Device) {}
+
+  public onSdkVersion(): void {
+    console.log(`sdkVersion: ${this.device.sdkVersion}`);
+  }
 }
