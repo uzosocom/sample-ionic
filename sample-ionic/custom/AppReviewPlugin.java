f2a70fc73bbb1aa4f8a29aa559b87644327cc284
uzosocom <7zu6s87y9fku@gmail.com>
2022-10-27 21:33:02 +0900

    fix: app-review

--- sample-ionic/custom/AppReviewPlugin.java
+++ sample-ionic/custom/AppReviewPlugin.java
@@ -1,12 +1,12 @@
 package by.chemerisuk.cordova;
 
 import static com.google.android.gms.tasks.Tasks.await;
+import static by.chemerisuk.cordova.support.ExecutionThread.WORKER;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 
-import com.google.android.gms.tasks.Task;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
@@ -19,19 +19,13 @@ import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
 
 public class AppReviewPlugin extends ReflectiveCordovaPlugin {
-    @CordovaMethod
+    @CordovaMethod(WORKER)
     private void requestReview(CallbackContext callbackContext) throws Exception {
         Activity activity = cordova.getActivity();
         ReviewManager manager = ReviewManagerFactory.create(activity);
         ReviewInfo reviewInfo = await(manager.requestReviewFlow());
-        Task<Void> flow = manager.launchReviewFlow(activity, reviewInfo);
-        flow.addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                callbackContext.success();
-            } else {
-                respondWith(task.getException(), callbackContext);
-            }
-        });
+        await(manager.launchReviewFlow(activity, reviewInfo));
+        callbackContext.success();
     }
 
     @CordovaMethod
@@ -44,9 +38,5 @@ public class AppReviewPlugin extends ReflectiveCordovaPlugin {
         intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
         cordova.getActivity().startActivity(intent);
         callbackContext.success();
-    }
-
-    private void respondWith(Exception e, CallbackContext callbackContext) {
-        callbackContext.error(e != null ? e.getMessage() : "Unknown error");
     }
 }
